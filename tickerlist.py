796f4764161a4e20d28ed1f71c9316386b391b94
Matthew Craig <16930669+FacelessHacker@users.noreply.github.com>
2021-09-16 19:40:55 -0700

    Added beginnings of separate TickerList class to transition to object-oriented design

---
+++ tickerlist.py
@@ -0,0 +1,27 @@
+class TickerList:
+
+    def __init__(self, name, iterable=None):
+        self._name = name
+        if iterable != None:
+            self._list = list(iterable)
+        else:
+            self._list = list()
+
+    def add_symbols(self, symbols):
+        """Add the given symbols to the list. All instances of '$' are stripped out, and empty strings are never added to the list."""
+        for s in symbols:
+            s = s.strip("$").upper()
+            if s: # Ignore empty strings silently
+                self._list.add(s)
+    
+    def remove_symbols(self, symbols):
+        for s in symbols:
+            s = s.strip("$").upper()
+            if s: # Ignore empty strings
+                if s in self._list:
+                    self._list.remove(s)
+                else:
+                    print(s + " not in list " + self._name)
+
+    def __iter__(self):
+        return iter(self._list)
